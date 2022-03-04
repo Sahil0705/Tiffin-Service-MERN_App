11c776c8dce12f2e7faa4e37a015f3a08919f9c1
Sahil <sahildonde7@gmail.com>
2022-03-04 16:28:52 +0530

    Cart update

--- src/Food_Cart/ContextCart.js
+++ src/Food_Cart/ContextCart.js
@@ -11,18 +11,18 @@ const ContextCart = () => {
       <><br/><br/>
         <header>
           <div className="continue-shopping">
-            <img src="./images/arrow.png" alt="arrow" className="arrow-icon" />
+            {/* <img src="./images/arrow.png" alt="arrow" className="arrow-icon" /> */}
             <h3>continue shopping</h3>
           </div>
 
           <div className="cart-icon">
-            <img src="./images/cart.png" alt="cart" />
+            {/* <img src="./images/cart.png" alt="cart" /> */}
             <p>{totalItem}</p>
           </div>
         </header>
 
         <section className="main-cart-section">
-          <h1>shopping Cart</h1>
+          <h1>shopping Cart (Project in Progress...)</h1>
           <p className="total-items">
             you have <span className="total-items-count">{totalItem} </span>{" "}
             items in shopping cart
@@ -36,18 +36,18 @@ const ContextCart = () => {
     <>
       <header><br/><br/><br/>
         <div className="continue-shopping">
-          <img src="arrow.png" alt="arrow" className="arrow-icon" />
+          {/* <img src="arrow.png" alt="arrow" className="arrow-icon" /> */}
           <h3>Home</h3>
         </div>
 
         <div className="cart-icon">
-          <img src="./cart.png" alt="cart" />
+          {/* <img src="./cart.png" alt="cart" /> */}
           <p>{totalItem}</p>
         </div>
       </header>
 
       <section className="main-cart-section">
-        <h1>shopping Cart</h1>
+        <h1>shopping Cart (Project in Progress...)</h1>
         <p className="total-items">
           you have <span className="total-items-count">{totalItem} </span> items
           in shopping cart
